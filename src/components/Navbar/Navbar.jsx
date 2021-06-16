6c0bd3b508e7710fa885c05ce7cd91b56aaf1ad7
cristian-hr <cristian.hrauh@gmail.com>
2021-06-16 18:08:28 -0400

    home "/" del

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
 
     return (
         <div className="navBar">
-            <Link className="linksClass" to="/portfolio/">Home</Link>
+            <Link className="linksClass" to="/portfolio">Home</Link>
             <span className="barNB">|</span>
             <Link className="linksClass" to="/portfolio/projects">Projects</Link>
             <span className="barNB">|</span>
