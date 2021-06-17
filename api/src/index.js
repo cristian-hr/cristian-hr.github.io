d6db6526d423f133a515db9653eab6d5ad33f08e
cristian-hr <cristian.hrauh@gmail.com>
2021-06-17 13:53:48 -0400

    back to http

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,5 @@
 import server from './app';
 
-var fs = require('fs');
-var https = require('https');
-
-var privateKey  = fs.readFileSync(__dirname+'/server.key', 'utf8');
-var certificate = fs.readFileSync(__dirname+'/server.pem', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
-
-var httpsServer = https.createServer(credentials, server);
-
-httpsServer.listen(process.env.PORT || 3001, () => {
+server.listen(process.env.PORT || 3001, () => {
   console.log(`API listening at ${process.env.PORT || 3001}`); // eslint-disable-line no-console
 });
