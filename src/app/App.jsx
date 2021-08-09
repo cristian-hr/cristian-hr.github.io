5937ad552744e828ceb0a5bab7a9c6e154ca517f
cristian-hr <cristian.hrauh@gmail.com>
2021-08-08 20:16:53 -0400

    lint

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import "../scss/App.scss";
 import Skills from "../components/Skills/Skills";
 import Contact from "../components/Contact/Contact";
